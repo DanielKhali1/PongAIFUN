6fe62ca9d73a13d9970a67a57ea0f23e0db10d34
moo32 <38340314+moo32@users.noreply.github.com>
2019-05-30 14:21:04 -0400

    Update MainGUI.java

--- src/GUI/MainGUI.java
+++ src/GUI/MainGUI.java
@@ -8,8 +8,11 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -28,8 +31,12 @@ public class MainGUI extends Application
 	
 	Rectangle paddle1 = new Rectangle(30, 130);
 	Rectangle paddle2 = new Rectangle(30, 130);
-	Pane pane = new Pane();
-	Scene scene = new Scene(pane, 1000, 800);
+	Pane gamePane = new Pane();
+	Scene scene = new Scene(gamePane, 1000, 800);
+	
+	Text ballSpeedTxt;
+	Text numberOfBouncesTxt;
+	int numberOfBounces = 0;
 	
 	Button settingsButton = new Button("  i  ");
 	
@@ -43,54 +50,158 @@ public class MainGUI extends Application
 	private boolean up = false;
 	private boolean down = false;
 	
+	Pane menuPane = new Pane();
+	
 	double movingx = 0;
 	double movingy = 0;
 	
 	public int humanScore = 0;
 
+	Pane dqnSetupPane;
+	boolean dqnPaneCreated = false;
+	
+	boolean dynamic = false;
+	boolean train = false;
+	boolean save = false;
+
+	
 	
 	public void start(Stage primaryStage)
 	{
-		initializeGameObjects();
 
 		
 		primaryStage.setTitle("Pong DQN");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
-		timeline.setCycleCount(Timeline.INDEFINITE);
-		timeline.getKeyFrames().add(KeyFrameContent(Duration.millis(40)));
-		timeline.play();
+		primaryStage.setResizable(false);		
+
 		
-	}
+		initializeGameObjects();
+		initializeMenu();
 	
+	}
+
+	
+	private void initializeMenu() 
+	{
+		menuPane.setPrefSize(1000, 800);
+		menuPane.setStyle("-fx-background-color: 'black';");
+		
+		Text pongTitle = new Text("PONG");
+		pongTitle.setStyle("-fx-font-size: 75;");
+		pongTitle.setFill(Color.WHITE);
+		pongTitle.setLayoutX(425);
+		pongTitle.setLayoutY(175);
+		menuPane.getChildren().add(pongTitle);
+		
+		Button playAgainstStaticAI = new Button("Play Against Static AI");
+		playAgainstStaticAI.setLayoutX(370);
+		playAgainstStaticAI.setLayoutY(250);
+		playAgainstStaticAI.setStyle("-fx-background-color: 'black'; -fx-text-fill: 'white'; -fx-border-color: white; -fx-font-size: 30; -fx-padding: 25;");
+		
+		playAgainstStaticAI.setOnMouseEntered(e->
+		{
+			playAgainstStaticAI.setStyle("-fx-background-color: 'white'; -fx-text-fill: 'black'; -fx-border-color: grey; -fx-font-size: 30; -fx-padding: 25;");
+		});
+		playAgainstStaticAI.setOnMouseExited(e->
+		{
+			playAgainstStaticAI.setStyle("-fx-background-color: 'black'; -fx-text-fill: 'white'; -fx-border-color: white; -fx-font-size: 30; -fx-padding: 25;");
+		});
+		playAgainstStaticAI.setOnAction(e->
+		{
+			menuPane.setVisible(false);
+			timeline.play();
+		});
+		menuPane.getChildren().add(playAgainstStaticAI);		
+
+		Button playAgainstDQN = new Button("Play Against DQN AI");
+		playAgainstDQN.setLayoutX(370);
+		playAgainstDQN.setLayoutY(400);
+		playAgainstDQN.setStyle("-fx-background-color: 'black'; -fx-text-fill: 'white'; -fx-border-color: white; -fx-font-size: 30; -fx-padding: 25;");
+		
+		playAgainstDQN.setOnMouseEntered(e->
+		{
+			playAgainstDQN.setStyle("-fx-background-color: 'white'; -fx-text-fill: 'black'; -fx-border-color: grey; -fx-font-size: 30; -fx-padding: 25;");
+		});
+		playAgainstDQN.setOnMouseExited(e->
+		{
+			playAgainstDQN.setStyle("-fx-background-color: 'black'; -fx-text-fill: 'white'; -fx-border-color: white; -fx-font-size: 30; -fx-padding: 25;");
+		});
+		playAgainstDQN.setOnAction(e->
+		{
+			dynamic = true;
+			menuPane.setVisible(false);
+			
+			if(!dqnPaneCreated)
+			{
+				createDqnPane();
+			}
+			
+			dqnSetupPane.setVisible(true);
+
+			
+			//timeline.play();
+		});
+		menuPane.getChildren().add(playAgainstDQN);
+		
+		
+		
+		
+		gamePane.getChildren().add(menuPane);
+	}
+
 	public KeyFrame KeyFrameContent(Duration duration)
 	{
 		KeyFrame keyFrame = new KeyFrame(duration, action ->
 		{
-			/*try {
-				Thread.sleep(speed);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}*/
-			// Boolean Value that Determines whether you can go back on top of yourself
-
-			//clears the display grid
-			
-			/*agent1.step();
-			
-	
-			if(agent1.scored())
-			{
-				iteration++;
-				agent1.reset();
-			}
-			*/
 			
 			realPlayerMovement();
 			
-			if(ball.getLayoutX() > 1000)
+			
+			//ai paddle
+			if(paddle1.getLayoutY()+65 > ball.getLayoutY() && paddle1.getLayoutY() >= 0)
+			{
+		        paddle1.setLayoutY(paddle1.getLayoutY() - 10);
+			}
+			else if(paddle1.getLayoutY()+65 < ball.getLayoutY() && paddle1.getLayoutY() + 130 <= 950)
+			{
+		        paddle1.setLayoutY(paddle1.getLayoutY() + 10);
+			}
+			
+			
+			
+			
+			if(ball.getLayoutY() > 800 || ball.getLayoutY() < 0)
+			{
+				movingy *= -1;
+			}
+			
+			if(ball.getLayoutY() > paddle2.getLayoutY() && ball.getLayoutY() < paddle2.getLayoutY()+130 && ball.getLayoutX() >= 950 )
+			{
+				if(Math.abs(movingx) >= 600)
+					movingx *= -1;
+				else
+				{
+					movingx *= -1.2;
+					movingy *= 1.2;
+				}
+				
+				numberOfBounces++;
+			}
+			else if(ball.getLayoutY() > paddle1.getLayoutY() && ball.getLayoutY() < paddle1.getLayoutY()+130 && ball.getLayoutX() <= 45)
+			{
+				if(Math.abs(movingx) >= 600)
+					movingx *= -1;
+				else
+				{
+					movingx *= -1.2;
+					movingx *= 1.2;
+				}
+				
+				numberOfBounces++;
+			}
+			else if(ball.getLayoutX() > 1000)
 			{
 				agent1.setScore(agent1.getScore()+1);
 				resetBall();
@@ -101,11 +212,6 @@ public class MainGUI extends Application
 				resetBall();
 			}
 			
-			if(ball.getLayoutY() > 800 || ball.getLayoutY() < 0)
-			{
-				//movingx *= -1;
-				movingy *= -1;
-			}
 			
 			ball.setLayoutX(movingx + ball.getLayoutX());
 			ball.setLayoutY(movingy + ball.getLayoutY());			
@@ -113,13 +219,253 @@ public class MainGUI extends Application
 			//adds to score if snake eats objective item
 			Player1Score.setText("Score: \t" + agent1.getScore());
 			Player2Score.setText("Score: \t" + humanScore);
+			
+			ballSpeedTxt.setText("Ball Speed: " + Math.abs((int)movingx));
+			numberOfBouncesTxt.setText("Bounces: " + numberOfBounces);
 		    
 		    //---------------------------- AI Integration ------------------------------- //
+			
 
 
 		});
 		
 		return keyFrame;
+	}
+	
+	public void createDqnPane()
+	{
+		dqnPaneCreated = true;
+		dqnSetupPane = new Pane();
+		dqnSetupPane.setStyle("-fx-background-color: 'black'");
+		dqnSetupPane.setPrefSize(1050, 850);
+
+		
+		
+		Text dqnSetupTitle = new Text("DQN Setup");
+		dqnSetupTitle.setLayoutX(400);
+		dqnSetupTitle.setLayoutY(150);
+		dqnSetupTitle.setFill(Color.WHITE);
+		dqnSetupTitle.setStyle("-fx-font-size: 50;");
+		dqnSetupPane.getChildren().add(dqnSetupTitle);
+		
+		Button btnBack = new Button(" < ");
+		btnBack.setLayoutX(20);
+		btnBack.setLayoutY(20);
+		btnBack.setStyle("-fx-background-color: 'black'; -fx-text-fill: 'white'; -fx-border-color: white; -fx-font-size: 30; -fx-padding: 10;");
+		
+		btnBack.setOnMouseEntered(a->
+		{
+			btnBack.setStyle("-fx-background-color: 'white'; -fx-text-fill: 'black'; -fx-border-color: grey; -fx-font-size: 30; -fx-padding: 10;");
+		});
+		btnBack.setOnMouseExited(a->
+		{
+			btnBack.setStyle("-fx-background-color: 'black'; -fx-text-fill: 'white'; -fx-border-color: white; -fx-font-size: 30; -fx-padding: 10;");
+		});
+		
+		btnBack.setOnAction(a ->
+		{
+			dqnSetupPane.setVisible(false);
+			menuPane.setVisible(true);
+		});
+		
+		dqnSetupPane.getChildren().add(btnBack);
+		
+		Text txtAgents = new Text("Agents");
+		
+		txtAgents.setLayoutX(150);
+		txtAgents.setLayoutY(250);
+		txtAgents.setFill(Color.WHITE);
+		txtAgents.setStyle("-fx-font-size: 25;");
+		dqnSetupPane.getChildren().add(txtAgents);
+		
+		
+		ScrollPane scroll = new ScrollPane();
+		VBox scrollMeat = new VBox();
+		scrollMeat.setStyle("-fx-background-color: 'black';");
+		scroll.setContent(scrollMeat);
+		scrollMeat.setPrefSize(300, 450);
+		scroll.setStyle("-fx-border-color: 'white'");
+		
+		scroll.setLayoutX(50);
+		scroll.setLayoutY(270);
+		dqnSetupPane.getChildren().add(scroll);
+		
+		gamePane.getChildren().add(dqnSetupPane);
+		
+		Pane createNewPane = new Pane();
+		createNewPane.setPrefSize(570, 450);
+		createNewPane.setLayoutX(400);
+		createNewPane.setLayoutY(270);
+		createNewPane.setStyle("-fx-border-color: 'grey'; -fx-background-color: 'black'; -fx-border-width: 2;");
+		dqnSetupPane.getChildren().add(createNewPane);
+		
+		Button btnCreateNew = new Button("Create New Agent");
+		btnCreateNew.setLayoutX(400);
+		btnCreateNew.setLayoutY(200);
+		btnCreateNew.setStyle("-fx-background-color: 'black'; -fx-text-fill: 'white'; -fx-border-color: white; -fx-font-size: 20; -fx-padding: 10;");
+		dqnSetupPane.getChildren().add(btnCreateNew);
+		
+		btnCreateNew.setOnMouseEntered( e ->
+		{
+			btnCreateNew.setStyle("-fx-background-color: 'white'; -fx-text-fill: 'black'; -fx-border-color: white; -fx-font-size: 20; -fx-padding: 10;");
+		});
+		
+		btnCreateNew.setOnMouseExited( e ->
+		{
+			btnCreateNew.setStyle("-fx-background-color: 'black'; -fx-text-fill: 'white'; -fx-border-color: white; -fx-font-size: 20; -fx-padding: 10;");
+		});
+		
+
+		
+		
+		Text txtDQNPar = new Text("DQN Parameters");
+		txtDQNPar.setLayoutX(20);
+		txtDQNPar.setLayoutY(40);
+		txtDQNPar.setStyle("-fx-font-size: 20;");
+		txtDQNPar.setFill(Color.WHITE);
+		createNewPane.getChildren().add(txtDQNPar);
+		
+		Text txtEpsilonDecay = new Text("Epsilon Decay");
+		txtEpsilonDecay.setLayoutX(20);
+		txtEpsilonDecay.setLayoutY(70+20);
+		txtEpsilonDecay.setFill(Color.WHITE);
+		createNewPane.getChildren().add(txtEpsilonDecay);
+		
+		TextField tfEpsilonDecay = new TextField();
+		tfEpsilonDecay.setLayoutX(20);
+		tfEpsilonDecay.setLayoutY(80+20);
+		tfEpsilonDecay.setStyle("-fx-border-color: 'black'");
+		createNewPane.getChildren().add(tfEpsilonDecay);
+		tfEpsilonDecay.setDisable(true);
+		
+		Text txtLearningRate = new Text("Learning Rate");
+		txtLearningRate.setLayoutX(20);
+		txtLearningRate.setLayoutY(130+20);
+		txtLearningRate.setFill(Color.WHITE);
+		createNewPane.getChildren().add(txtLearningRate);
+		
+		TextField tfLearningRate = new TextField();
+		tfLearningRate.setLayoutX(20);
+		tfLearningRate.setLayoutY(140+20);
+		tfLearningRate.setStyle("-fx-border-color: 'black'");
+		createNewPane.getChildren().add(tfLearningRate);
+		tfLearningRate.setDisable(true);
+		
+		Text txtDiscountFactor = new Text("Discount Factor");
+		txtDiscountFactor.setLayoutX(20);
+		txtDiscountFactor.setLayoutY(190+20);
+		txtDiscountFactor.setFill(Color.WHITE);
+		createNewPane.getChildren().add(txtDiscountFactor);
+		
+		TextField tfDiscountFactor = new TextField();
+		tfDiscountFactor.setLayoutX(20);
+		tfDiscountFactor.setLayoutY(200+20);
+		tfDiscountFactor.setStyle("-fx-border-color: 'black'");
+		createNewPane.getChildren().add(tfDiscountFactor);
+		tfDiscountFactor.setDisable(true);
+		
+		Text txtRewards = new Text("Rewards");
+		txtRewards.setLayoutX(300);
+		txtRewards.setLayoutY(40);
+		txtRewards.setStyle("-fx-font-size: 20;");
+		txtRewards.setFill(Color.WHITE);
+		createNewPane.getChildren().add(txtRewards);
+		
+		
+		Text txtHittingBall = new Text("Hitting Ball");
+		txtHittingBall.setLayoutX(300);
+		txtHittingBall.setLayoutY(70+20);
+		txtHittingBall.setFill(Color.WHITE);
+		createNewPane.getChildren().add(txtHittingBall);
+		
+		TextField tfHittingBall = new TextField();
+		tfHittingBall.setLayoutX(300);
+		tfHittingBall.setLayoutY(80+20);
+		tfHittingBall.setStyle("-fx-border-color: 'black'");
+		createNewPane.getChildren().add(tfHittingBall);
+		tfHittingBall.setDisable(true);
+		
+		Text txtScoredOn = new Text("Scored On");
+		txtScoredOn.setLayoutX(300);
+		txtScoredOn.setLayoutY(130+20);
+		txtScoredOn.setFill(Color.WHITE);
+		createNewPane.getChildren().add(txtScoredOn);
+		
+		TextField tfScoredOn = new TextField();
+		tfScoredOn.setLayoutX(300);
+		tfScoredOn.setLayoutY(140+20);
+		tfScoredOn.setStyle("-fx-border-color: 'black'");
+		createNewPane.getChildren().add(tfScoredOn);
+		tfScoredOn.setDisable(true);
+		
+		Text txtScoring = new Text("Scoring");
+		txtScoring.setLayoutX(300);
+		txtScoring.setLayoutY(190+20);
+		txtScoring.setFill(Color.WHITE);
+		createNewPane.getChildren().add(txtScoring);
+		
+		TextField tfScoring = new TextField();
+		tfScoring.setLayoutX(300);
+		tfScoring.setLayoutY(200+20);
+		tfScoring.setStyle("-fx-border-color: 'black'");
+		createNewPane.getChildren().add(tfScoring);
+		tfScoring.setDisable(true);
+		
+		
+		Button btnTrain = new Button("Train");
+		btnTrain.setLayoutX(350);
+		btnTrain.setLayoutY(350);
+		btnTrain.setStyle("-fx-background-color: 'black'; -fx-text-fill: 'white'; -fx-border-color: white; -fx-font-size: 20; -fx-padding: 10;");
+		createNewPane.getChildren().add(btnTrain);
+		
+		
+		btnTrain.setOnMouseEntered( e ->
+		{
+			if(train)
+				btnTrain.setStyle("-fx-background-color: 'white'; -fx-text-fill: 'black'; -fx-border-color: white; -fx-font-size: 20; -fx-padding: 10;");
+		});
+		
+		btnTrain.setOnMouseExited( e ->
+		{
+			if(train)
+				btnTrain.setStyle("-fx-background-color: 'black'; -fx-text-fill: 'white'; -fx-border-color: white; -fx-font-size: 20; -fx-padding: 10;");
+		});
+		
+		
+		Button btnSave = new Button("Save");
+		btnSave.setLayoutX(450);
+		btnSave.setLayoutY(350);
+		btnSave.setStyle("-fx-background-color: 'black'; -fx-text-fill: 'white'; -fx-border-color: white; -fx-font-size: 20; -fx-padding: 10;");
+		createNewPane.getChildren().add(btnSave);
+		
+		btnSave.setOnMouseEntered( e ->
+		{
+			btnSave.setStyle("-fx-background-color: 'white'; -fx-text-fill: 'black'; -fx-border-color: white; -fx-font-size: 20; -fx-padding: 10;");
+		});
+		
+		btnSave.setOnMouseExited( e ->
+		{
+			btnSave.setStyle("-fx-background-color: 'black'; -fx-text-fill: 'white'; -fx-border-color: white; -fx-font-size: 20; -fx-padding: 10;");
+		});
+		
+		btnCreateNew.setOnAction(e -> {
+			createNewPane.setStyle("-fx-border-color: 'white'; -fx-background-color: 'black'; -fx-border-width: 2;");
+			scroll.setStyle("-fx-border-color: 'black';");
+			tfScoring.setDisable(false);
+			tfScoredOn.setDisable(false);
+			tfHittingBall.setDisable(false);
+			tfDiscountFactor.setDisable(false);
+			tfLearningRate.setDisable(false);
+			tfEpsilonDecay.setDisable(false);
+			
+			train = true;
+
+
+		});
+		
+		
+		
+		
 	}
 	
 	
@@ -149,24 +495,40 @@ public class MainGUI extends Application
 		});
 		
 		
-		if(up)
+		if(up && paddle2.getLayoutY() > 0)
 		{
 	        paddle2.setLayoutY(paddle2.getLayoutY() - 15);
 		}
-		else if(down)
+		else if(down && paddle2.getLayoutY() < 650)
 		{
 			paddle2.setLayoutY(paddle2.getLayoutY() + 15);
 		}
+		
+		
+		gamePane.setOnMouseMoved(e ->
+		{
+			paddle2.setLayoutY(e.getY());
+		});
+	
 		
 	}
 	
 	public void resetBall()
 	{
 		
+		movingy = randomBetween(4, 8);
+		movingx = 9 - movingy;
 		
-		movingx = randomBetween(0, speed*2);
-		movingy = 2 * speed - movingx;
-		movingx -= speed;
+		movingx *= 2;
+		
+		double coinFlip = randomBetween(1, 2);
+		
+		if(coinFlip > 1.5)
+		{
+			movingx *= -1;
+		}
+		
+		
 		
 		ball.setLayoutX(500);
 		ball.setLayoutY(375);
@@ -182,40 +544,82 @@ public class MainGUI extends Application
 	
 	public void initializeGameObjects()
 	{
-		pane.setStyle("-fx-background-color: 'black';");
+		timeline.setCycleCount(Timeline.INDEFINITE);
+		timeline.getKeyFrames().add(KeyFrameContent(Duration.millis(40)));
+		
+		gamePane.setStyle("-fx-background-color: 'black';");
 		
 		Player1Score.setLayoutX(100);
 		Player1Score.setLayoutY(750);
 		Player1Score.setFill(Color.WHITE);
 		Player1Score.setStyle("-fx-font-size: 16;");
-		pane.getChildren().add(Player1Score);
+		gamePane.getChildren().add(Player1Score);
 		
 		Player2Score.setLayoutX(800);
 		Player2Score.setLayoutY(750);
 		Player2Score.setFill(Color.WHITE);
 		Player2Score.setStyle("-fx-font-size: 16;");
-		pane.getChildren().add(Player2Score);
+		gamePane.getChildren().add(Player2Score);
 		
 
 		ball.setFill(Color.WHITE);
-		pane.getChildren().add(ball);
+		gamePane.getChildren().add(ball);
 		
 		
 		paddle1.setLayoutX(20);
 		paddle1.setLayoutY(300);
 		paddle1.setFill(Color.WHITE);
-		pane.getChildren().add(paddle1);
+		gamePane.getChildren().add(paddle1);
 		
 		paddle2.setLayoutX(950);
 		paddle2.setLayoutY(300);
 		paddle2.setFill(Color.WHITE);
-		pane.getChildren().add(paddle2);
+		gamePane.getChildren().add(paddle2);
 		
-		settingsButton.setLayoutX(480);
+		Button exitButton = new Button(" X ");
+		exitButton.setStyle("-fx-background-color: '#494949'; -fx-text-fill: 'white'; -fx-padding: 10;");
+		exitButton.setLayoutX(430);
+		exitButton.setLayoutY(741);
+		gamePane.getChildren().add(exitButton);
+		
+		exitButton.setOnMouseEntered(e->{
+			exitButton.setStyle("-fx-background-color: 'red'; -fx-text-fill: 'white'; -fx-padding: 11;");
+		});
+		
+		exitButton.setOnMouseExited(e->{
+			exitButton.setStyle("-fx-background-color: '#494949'; -fx-text-fill: 'white'; -fx-padding: 10;");
+		});
+		
+		exitButton.setOnMouseClicked(e->{
+			exitButton.setStyle("-fx-background-color: '#303030'; -fx-text-fill: 'darkgrey'; -fx-padding: 10;");
+			
+			timeline.stop();
+			
+			//probably should put a "are you sure?" popup
+			
+			menuPane.setVisible(true);
+		});
+		
+		settingsButton.setLayoutX(470);
 		settingsButton.setLayoutY(740);
 		settingsButton.setStyle("-fx-background-color: '#494949'; -fx-text-fill: 'white'; -fx-padding: 10;");
+		gamePane.getChildren().add(settingsButton);
 
-		pane.getChildren().add(settingsButton);
+		
+		ballSpeedTxt = new Text("Ball Speed: " + Math.abs((int)movingx));
+		ballSpeedTxt.setLayoutX(470);
+		ballSpeedTxt.setLayoutY(20);
+		ballSpeedTxt.setStyle("-fx-font-size: 10");
+		ballSpeedTxt.setFill(Color.WHITE);
+		gamePane.getChildren().add(ballSpeedTxt);
+
+		
+		numberOfBouncesTxt = new Text("Bounces: " + numberOfBounces);
+		numberOfBouncesTxt.setLayoutX(400);
+		numberOfBouncesTxt.setLayoutY(20);
+		numberOfBouncesTxt.setStyle("-fx-font-size: 10");
+		numberOfBouncesTxt.setFill(Color.WHITE);
+		gamePane.getChildren().add(numberOfBouncesTxt);
 		
 		resetBall();
 		
@@ -250,8 +654,7 @@ public class MainGUI extends Application
 		
 		
 		
-		
-		
+
 	}
 	
 	public static void main(String[] args){launch(args);}
